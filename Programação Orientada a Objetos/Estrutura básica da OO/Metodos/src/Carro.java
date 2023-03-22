378fcc8e7770da6433ed4deb0f3a83d0ec1564e2
Deividi Filho <82412889+DeividiFilho@users.noreply.github.com>
2023-03-22 19:07:15 -0300

    Add files via upload

---
+++ Programação Orientada a Objetos/Estrutura básica da OO/Metodos/src/Carro.java
@@ -0,0 +1,37 @@
+public class Carro {
+        private String cor;
+        private String modelo;
+        private int capacidadeTanque;
+
+    public Carro(String cor, String modelo, int capacidadeTanque) {
+        this.setCor(cor);
+        this.setModelo(modelo);
+        this.setCapacidadeTanque(capacidadeTanque);
+    }
+
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+}
