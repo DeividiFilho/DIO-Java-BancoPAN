1f7c5cd492756aa76c7d54009c32e789e8965122
Deividi Filho <82412889+DeividiFilho@users.noreply.github.com>
2023-03-26 19:19:57 -0300

    Update ExemploList.java

--- Programação Orientada a Objetos/Collections/List/src/ExemploList.java
+++ Programação Orientada a Objetos/Collections/List/src/ExemploList.java
@@ -2,18 +2,6 @@ import java.util.*;
 
 class ExemploList {
         public static void main(String[] args) {
-// Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
-
-//      List notas = new ArrayList(); //antes do java 5
-//      ArrayList<Double> notas = new ArrayList<>();
-//      List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
-        /*List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(10d);
-        System.out.println(notas);*/
-        /*List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
-        notas.add(1d);
-        notas.remove(5d);
-        System.out.println(notas);*/
 
             System.out.println("Crie uma lista e adicione as sete notas: ");
 
@@ -38,9 +26,6 @@ class ExemploList {
             System.out.println(notas);
 
             System.out.println("Confira se a nota 5.0 está na lista: " + notas.contains(5d));
-
-        /*System.out.println("Exiba todas as notas na ordem em que foram informados: ");
-        for (Double nota : notas) System.out.println(nota);*/
 
             System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
             System.out.println(notas.toString());
@@ -75,19 +60,7 @@ class ExemploList {
             }
             System.out.println(notas);
 
-        /*System.out.println("Apague toda a lista");
-        notas.clear();
-        System.out.println(notas);
-        */
             System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
-
-/*
-Para você: Resolva esses exercícios utilizando os métodos da implementação LinkedList:
-        System.out.println("Crie uma lista chamada notas2 " +
-                "e coloque todos os elementos da list Arraylist nessa nova lista: ");
-        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: ");
-        System.out.println("Mostre a primeira nota da nova lista removendo-o: ");
-*/
 
         }
 }
